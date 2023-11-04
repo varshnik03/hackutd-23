efcb9d54e26a9dfe7d97f6e4a72c1760ad8ec1fe
Varshni <varshnik@gmail.com>
2023-11-04 18:08:15 -0500

    updateApp.js

--- fmae-housing/src/App.js
+++ fmae-housing/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React TODAY
         </a>
         <button type="button" class="text-white bg-blue-700 hover:bg-blue-800   font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
